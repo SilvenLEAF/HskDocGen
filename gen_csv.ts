2b6e0b62f281669f305453dee0fbb713290c07b9
SilvenLEAF <SilvenLEAF@gmail.com>
2023-09-16 23:32:02 +0530

    feat(hsk5): 900

--- gen_csv.ts
+++ gen_csv.ts
@@ -6,10 +6,10 @@ const randomizeList = (list: any[]) => list.sort(() => (Math.random() > .5) ? 1 
 const main = async () => {
   try {
     const main_list = hsk_list;
-    const records = main_list.slice(600, 800);
+    const records = main_list.slice(800, 1000);
     const totalItems = records.length;
 
-    const xlist1 = records.slice(0, 200);
+    const xlist1 = records.slice(0, 100);
     // randomizeList(xlist1);
     const csvList: any[] = [];
     for (let i = 0; i < xlist1.length; i++) {
@@ -20,7 +20,7 @@ const main = async () => {
     }
 
     const rootFol = ``;
-    const fileName = `hai_long_douluo_san`;
+    const fileName = `hao_tian_douluo_ling`;
     const fileResp = await createCSV({ dataList: csvList, rootFol: rootFol, fileName: fileName });
 
     console.log(",fileResp", fileResp);
