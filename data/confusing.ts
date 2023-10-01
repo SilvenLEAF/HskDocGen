5a474767b27842561f03f68351b633c36487afea
SilvenLEAF <SilvenLEAF@gmail.com>
2023-10-01 06:32:20 +0530

    _soul_II

---
+++ data/confusing.ts
@@ -0,0 +1,13 @@
+export const industry_list = [
+  { slNo: 1, hanzi: "行业", pinyin: "hángyè", meaning: "IT Industry, Service Industry, Hotel Industry etc" },
+  { slNo: 2, hanzi: "工业", pinyin: "gōngyè", meaning: "manufacture industry (vs 农业 vs 商业). Coal Industry, Steel Industry, Car Industry" },
+  { slNo: 3, hanzi: "企业", pinyin: "qǐyè", meaning: "Tencent Enterprise (enterpreneur)" },
+  { slNo: 4, hanzi: "业界", pinyin: "àn", meaning: "(Tencent retained 1st spot) in the circle of this industry)" },
+]
+
+export const hsk_list = [
+  { slNo: 1, hanzi: "行业", pinyin: "hángyè", meaning: "IT Industry, Service Industry, Hotel Industry etc" },
+  { slNo: 2, hanzi: "工业", pinyin: "gōngyè", meaning: "manufacture industry (vs 农业 vs 商业). Coal Industry, Steel Industry, Car Industry" },
+  { slNo: 3, hanzi: "企业", pinyin: "qǐyè", meaning: "Tencent Enterprise (enterpreneur)" },
+  { slNo: 4, hanzi: "业界", pinyin: "àn", meaning: "(Tencent retained 1st spot) in the circle of this industry)" },
+]
