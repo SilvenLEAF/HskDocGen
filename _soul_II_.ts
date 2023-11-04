321d90ce9167dc8ea59d7e638e8e46e1b1732d3e
SilvenLEAF <SilvenLEAF@gmail.com>
2023-11-04 22:09:31 +0530

    _soul_II

---
+++ _soul_II_.ts
@@ -0,0 +1,46 @@
+const _soul_II_ = {
+  xiu_luo_dou_luo: `修罗斗罗`,
+  ji_xian_dou_luo: `极限斗罗`,
+
+  feng_hao_dou_luo: `封号斗罗`,
+  hao_tian_dou_luo: `昊天斗罗`,
+  hai_long_dou_luo: `海龙斗罗`,
+  jian_dou_luo: `剑斗罗`,
+  gui_dou_luo: `鬼斗罗`,
+  hun_dou_luo: `魂斗罗`,
+
+  hun_sheng: `魂圣`,
+  hun_di: `魂帝`,
+  hun_wang: `魂王`,
+  hun_zong: `魂宗`,
+  hun_zun: `魂尊`,
+  da_hun_shi: `大魂师`,
+  hun_shi: `魂师`,
+}
+
+const _soul_II_sect_ = [
+  _soul_II_.feng_hao_dou_luo,
+  _soul_II_.hun_dou_luo,
+  _soul_II_.hun_sheng,
+  _soul_II_.hun_di,
+  _soul_II_.hun_wang,
+  _soul_II_.hun_zong,
+]
+
+const douluo_dalu_5000 = {
+  hun_250_x_10: '修罗斗罗',
+  feng_hao_douluo_250_x_05: {
+    __I: 'hun_douluo',
+    _II: 'gui_douluo',
+    III: 'jian_douluo',
+    _IV: 'hai_long_douluo',
+    __V: 'hao_tian_douluo',
+  },
+  yao_ling_shi_250_x_05: {
+    __I: 'qing_tong_yao_ling_shi',
+    _II: 'bai_yin_yao_ling_shi',
+    III: 'haung_jin_yao_ling_shi',
+    _IV: 'hei_jin_yao_ling_shi',
+    __V: 'quan_ji_yao_ling_shi',
+  },
+}
