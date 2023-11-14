652319847c8dbed57b5b1b53c6598e9c25560762
SilvenLEAF <SilvenLEAF@gmail.com>
2023-11-14 13:35:00 +0530

    _soul_II

--- gen_csv.ts
+++ gen_csv.ts
@@ -8,7 +8,7 @@ const randomizeList = (list: any[]) => list.sort(() => (Math.random() > .5) ? 1 
 const main = async () => {
   try {
     const main_list = hsk_list;
-    const records = main_list.slice(500, 550);
+    const records = main_list.slice(550, 600);
     const totalItems = records.length;
 
     const xlist1 = records;  // records.slice(200, 300);
@@ -24,8 +24,9 @@ const main = async () => {
     }
 
     const rootFol = `Di_LIU_Ji`;
-    const fileName = `封号斗罗第三魂环`;
+    // const fileName = `封号斗罗第三魂环`;
     // const fileName = `炼斗罗第一步`;
+    const fileName = `六百`;
     const fileResp = await createCSV({ dataList: csvList, rootFol: rootFol, fileName: fileName });
 
     console.log(",fileResp", fileResp);
